c6a901edf994ab281536932ca5a939d4e6b9ba6c
Damon Windsor <98252803+damonwindsor97@users.noreply.github.com>
2024-09-08 02:28:15 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Routes>
       <Route element={<Layout/>}>
-        <Route index path="/" element={<Home/>}/>
+        <Route index path="/" element={<Down/>}/>
 
         <Route path="*" element={<NotFound/>}/>
       </Route>
