34e65e7ba30e1e99039c7737eccfbec57752d612
Damon Windsor <damonwindsor@gmail.com>
2025-05-21 10:39:41 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <Routes>
       <Route element={<Layout/>}>
-        <Route index path="/" element={<Down/>}/>
+        <Route index path="/" element={<Home/>}/>
         <Route path="/privacypolicy" element={<PrivacyPolicy/>}/>
 
         <Route path="*" element={<NotFound/>}/>
