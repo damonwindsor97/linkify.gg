d40df7604c6f303f8a7a7911ae7d5169f475918c
Damon Windsor <damonwindsor@gmail.com>
2025-05-22 16:52:38 +1000

    socket fix

--- src/scripts/progressListener.js
+++ src/scripts/progressListener.js
@@ -2,23 +2,34 @@ import { useEffect } from 'react';
 import { socket } from './socket';
 
 const useProgressSocket = (onProgress) => {
-  useEffect(() => {
+    useEffect(() => {
     console.log('Attempting socket connection...');
 
     socket.on('connect', () => {
-      console.log('Socket connected:', socket.id);
+        console.log('[SOCKET] Connected! ID:', socket.id);
+    });
+
+    socket.on('connect_error', (err) => {
+        console.error('[SOCKET] Connection error:', err.message);
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.warn('[SOCKET] Disconnected. Reason:', reason);
     });
 
     socket.on('progress', (data) => {
-      console.log('Progress update:', data);
-      onProgress(data);
+        console.log('[SOCKET] Progress event:', data);
+        onProgress(data);
     });
 
     return () => {
-      socket.off('progress');
-      socket.off('connect');
+        console.log('Cleaning up socket listeners');
+        socket.off('connect');
+        socket.off('connect_error');
+        socket.off('disconnect');
+        socket.off('progress');
     };
-  }, [onProgress]);
+    }, [onProgress]);
 };
 
 export default useProgressSocket;
--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -2,5 +2,7 @@ import { io } from 'socket.io-client';
 
 // CHANGE ADDRESS TO API LIVE SERVER 
 export const socket = io('https://dev-media-download-api.onrender.com', {
-  transports: ['websocket'],
+    transports: ['websocket'],
+    autoConnect: true,
+    secure: true
 });
