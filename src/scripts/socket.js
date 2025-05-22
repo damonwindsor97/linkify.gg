c45a19324969eaeaa50accd3cba923700b7019ff
Damon Windsor <damonwindsor@gmail.com>
2025-05-22 17:09:37 +1000

    socket

--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -3,6 +3,4 @@ import { io } from 'socket.io-client';
 // CHANGE ADDRESS TO API LIVE SERVER 
 export const socket = io('https://dev-media-download-api.onrender.com', {
     transports: ['websocket'],
-    autoConnect: true,
-    secure: true
 });
