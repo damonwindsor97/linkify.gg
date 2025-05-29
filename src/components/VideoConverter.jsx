282e51ad0000d7d7fa306a45aabadc9ad6ae931d
Damon Windsor <damonwindsor@gmail.com>
2025-05-30 01:35:21 +1000

    testing

--- src/components/VideoConverter.jsx
+++ src/components/VideoConverter.jsx
@@ -82,7 +82,7 @@ function VIdeoConverter() {
 
       console.log('calling API')
       const response = await fetch(
-        `https://media-download-api.onrender.com/api/v1/video/tomp3`, {
+        `https://dev-media-download-api.onrender.com/api/v1/video/tomp3`, {
           method: 'POST',
           body: formData
         }
